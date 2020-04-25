62fa05d5e0e30411a7e6c4ed9cac705d37914e0f
BloodyNightWolf <64223721+BloodyNightWolf@users.noreply.github.com>
2020-04-25 13:57:22 -0400

    Mise à jour des objets/buttons

--- catalogue.php
+++ catalogue.php
@@ -24,23 +24,23 @@ require 'includes/headEtCSS.php';
         <div class="row">
           <div class="col-md-4">
            <div class="jumbotron">
-            <h4>Objet</h4>
-          <p>Description</p>
-            <p><button href="pageDetails.php" class="btn btn-info btn-sm">Voir</button></p>
+            <h4>Garfield Kart: Furious Racing</h4>
+          <p>Garfield, the famous lasagna-loving cat is back to take...</p>
+            <p><button href="pageDetails.php" class="btn btn-info btn-sm" value='1'>Voir</button></p>
            </div>
           </div>
           <div class="col-md-4">
             <div class="jumbotron">
-            <h4>Objet</h4>
-          <p>Description</p>
-            <p><button href="pageDetails.php" class="btn btn-info btn-sm">Voir</button></p>
+            <h4>Mordhau</h4>
+          <p>MORDHAU est un slasher multijoueur à la première et troisième personne basée au Moyen-age.</p>
+            <p><button href="pageDetails.php" class="btn btn-info btn-sm" value='2'>Voir</button></p>
             </div>
           </div>
           <div class="col-md-4">
            <div class="jumbotron">
-            <h4>Objet</h4>
-          <p>Description</p>
-            <p><button href="pageDetails.php" class="btn btn-info btn-sm">Voir</button></p>
+            <h4>Call of Duty 4: Modern Warfare</h4>
+          <p>Call of Duty 4: Modern Warfare est un jeu vidéo de tir à la première personne développé...</p>
+            <p><button href="pageDetails.php" class="btn btn-info btn-sm" value='3'>Voir</button></p>
            </div>
           </div>
         </div>
@@ -48,23 +48,23 @@ require 'includes/headEtCSS.php';
         <div class="row">
           <div class="col-md-4">
            <div class="jumbotron">
-            <h4>Objet</h4>
-          <p>Description</p>
-            <p><button href="pageDetails.php" class="btn btn-info btn-sm">Voir</button></p>
+            <h4>Halo: Reach</h4>
+          <p>Halo: Reach est un jeu vidéo de tir à la première personne (FPS) développé par Bungie et...</p>
+            <p><button href="pageDetails.php" class="btn btn-info btn-sm" value='4'>Voir</button></p>
            </div>
           </div>
           <div class="col-md-4">
             <div class="jumbotron">
-            <h4>Objet</h4>
-          <p>Description</p>
-            <p><button href="pageDetails.php" class="btn btn-info btn-sm">Voir</button></p>
+            <h4>Red Dead Redemption 2</h4>
+          <p>Amérique, 1899. L'ère de l'ouest sauvage touche à sa fin.</p>
+            <p><button href="pageDetails.php" class="btn btn-info btn-sm" value='5'>Voir</button></p>
             </div>
           </div>
           <div class="col-md-4">
            <div class="jumbotron">
-            <h4>Objet</h4>
-          <p>Description</p>
-            <p><button href="pageDetails.php" class="btn btn-info btn-sm">Voir</button></p>
+            <h4>Minecraft</h4>
+          <p>Minecraft est un jeu vidéo de type "bac à sable" (construction complètement libre) développé...</p>
+            <p><button href="pageDetails.php" class="btn btn-info btn-sm" value='6'>Voir</button></p>
            </div>
           </div>
         </div>
