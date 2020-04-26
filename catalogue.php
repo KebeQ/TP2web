749e55a84bdcec72c7324f26d73fab7f0f8f417d
BloodyNightWolf <64223721+BloodyNightWolf@users.noreply.github.com>
2020-04-26 15:53:46 -0400

    Ajout des images dans les boites

--- catalogue.php
+++ catalogue.php
@@ -25,6 +25,7 @@ require 'includes/headEtCSS.php';
           <div class="col-md-4">
            <div class="jumbotron">
             <h4>Garfield Kart: Furious Racing</h4>
+            <img src="img/Garfield.png" width="100" height="150">
           <p>Garfield, the famous lasagna-loving cat is back to take...</p>
             <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='1'>Voir</a></p>
            </div>
@@ -32,6 +33,7 @@ require 'includes/headEtCSS.php';
           <div class="col-md-4">
             <div class="jumbotron">
             <h4>Mordhau</h4>
+            <img src="img/Mordhau.jpg" width="100" height="150">
           <p>MORDHAU est un slasher multijoueur à la première et troisième personne basée au Moyen-age.</p>
             <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='2'>Voir</a></p>
             </div>
@@ -39,6 +41,7 @@ require 'includes/headEtCSS.php';
           <div class="col-md-4">
            <div class="jumbotron">
             <h4>Call of Duty 4: Modern Warfare</h4>
+            <img src="img/CallofDuty.jpg" width="100" height="150">
           <p>Call of Duty 4: Modern Warfare est un jeu vidéo de tir à la première personne développé...</p>
             <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='3'>Voir</a></p>
            </div>
@@ -49,6 +52,7 @@ require 'includes/headEtCSS.php';
           <div class="col-md-4">
            <div class="jumbotron">
             <h4>Halo: Reach</h4>
+            <img src="img/Halo.png" width="100" height="150">
           <p>Halo: Reach est un jeu vidéo de tir à la première personne (FPS) développé par Bungie et...</p>
             <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='4'>Voir</a></p>
            </div>
@@ -56,6 +60,7 @@ require 'includes/headEtCSS.php';
           <div class="col-md-4">
             <div class="jumbotron">
             <h4>Red Dead Redemption 2</h4>
+            <img src="img/RedDead.png" width="200" height="150">
           <p>Amérique, 1899. L'ère de l'ouest sauvage touche à sa fin.</p>
             <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='5'>Voir</a></p>
             </div>
@@ -63,6 +68,7 @@ require 'includes/headEtCSS.php';
           <div class="col-md-4">
            <div class="jumbotron">
             <h4>Minecraft</h4>
+            <img src="img/Minecraft.jpg" width="100" height="150">
           <p>Minecraft est un jeu vidéo de type "bac à sable" (construction complètement libre) développé...</p>
             <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='6'>Voir</a></p>
            </div>
