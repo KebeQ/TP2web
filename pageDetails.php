25c9c2bd8736e42cb8c0ed2798ef9d33a9d0bef7
KebeQ <45235498+KebeQ@users.noreply.github.com>
2020-04-30 19:11:33 -0400

    Update pageDetails.php

--- pageDetails.php
+++ pageDetails.php
@@ -1,5 +1,5 @@
 <?php
-require 'produit.class.php';
+require 'tables/produit.class.php';
 ?>
 
 <!DOCTYPE html>
@@ -7,13 +7,29 @@ require 'produit.class.php';
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style/pageDetails.css">
+<script src="js/pageDetails.js"></script> 
 <title>TP2</title>
 </head>
+
 <?php 
 require_once 'includes/connexion.php';
 ?>
-<body>
 
+<body onload="showText()">
+
+    <p id="jeux1">jeux1</p>
+
+    <p id="jeux2">jeux2</p>
+
+    <p id="jeux3">jeux3</p>
+
+    <p id="jeux4">jeux4</p>
+
+    <p id="jeux5">jeux5</p>
+
+    <p id="jeux6">jeux6</p>
 
 </body>
+
 </html>
