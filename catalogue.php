53c47a93cdc9d84e7e24f55daffb9d5ca8605b8e
KebeQ <45235498+KebeQ@users.noreply.github.com>
2020-04-30 19:10:53 -0400

    Update catalogue.php

--- catalogue.php
+++ catalogue.php
@@ -4,6 +4,7 @@ require 'tables/produit.class.php';
 
 <!DOCTYPE html>
 <html lang="en">
+<script src="js/catalogue.js"></script> 
 <?php 
 require_once 'includes/connexion.php';
 require_once 'includes/headEtCSS.php';
@@ -37,7 +38,7 @@ require_once 'tables/clientDAO.class.php';
             <h4>Garfield Kart: Furious Racing</h4>
             <img src="img/Garfield.png" width="100" height="150">
           <p>Garfield, the famous lasagna-loving cat is back to take...</p>
-            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='1'>Voir</a></p>
+            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='1' id="1" onclick="pageDetailsValue(this.id)">Voir</a></p>
            </div>
           </div>
           <div class="col-md-4">
@@ -45,7 +46,7 @@ require_once 'tables/clientDAO.class.php';
             <h4>Mordhau</h4>
             <img src="img/Mordhau.jpg" width="100" height="150">
           <p>MORDHAU est un slasher multijoueur à la première et troisième personne basée au Moyen-age.</p>
-            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='2'>Voir</a></p>
+            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='2' id="2" onclick="pageDetailsValue(this.id)">Voir</a></p>
             </div>
           </div>
           <div class="col-md-4">
@@ -53,7 +54,7 @@ require_once 'tables/clientDAO.class.php';
             <h4>Call of Duty 4: Modern Warfare</h4>
             <img src="img/CallofDuty.jpg" width="100" height="150">
           <p>Call of Duty 4: Modern Warfare est un jeu vidéo de tir à la première personne développé...</p>
-            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='3'>Voir</a></p>
+            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='3' id="3" onclick="pageDetailsValue(this.id)">Voir</a></p>
            </div>
           </div>
         </div>
@@ -64,7 +65,7 @@ require_once 'tables/clientDAO.class.php';
             <h4>Halo: Reach</h4>
             <img src="img/Halo.png" width="100" height="150">
           <p>Halo: Reach est un jeu vidéo de tir à la première personne (FPS) développé par Bungie et...</p>
-            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='4'>Voir</a></p>
+            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='4' id="4" onclick="pageDetailsValue(this.id)">Voir</a></p>
            </div>
           </div>
           <div class="col-md-4">
@@ -72,7 +73,7 @@ require_once 'tables/clientDAO.class.php';
             <h4>Red Dead Redemption 2</h4>
             <img src="img/RedDead.png" width="200" height="150">
           <p>Amérique, 1899. L'ère de l'ouest sauvage touche à sa fin.</p>
-            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='5'>Voir</a></p>
+            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='5' id="5" onclick="pageDetailsValue(this.id)">Voir</a></p>
             </div>
           </div>
           <div class="col-md-4">
@@ -80,10 +81,9 @@ require_once 'tables/clientDAO.class.php';
             <h4>Minecraft</h4>
             <img src="img/Minecraft.jpg" width="100" height="150">
           <p>Minecraft est un jeu vidéo de type "bac à sable" (construction complètement libre) développé...</p>
-            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='6'>Voir</a></p>
+            <p><a href="pageDetails.php" class="btn btn-info btn-sm" value='6' id="5" onclick="pageDetailsValue(this.id)">Voir</a></p>
            </div>
           </div>
-        </div>
-
+        </div>  
 </body>
 </html>
