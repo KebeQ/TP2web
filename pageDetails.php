0117f533b110c3adbebe8fdca944b62f73afe278
KebeQ <45235498+KebeQ@users.noreply.github.com>
2020-05-05 14:00:19 -0400

    Update pageDetails.php

--- pageDetails.php
+++ pageDetails.php
@@ -17,19 +17,83 @@ require_once 'includes/connexion.php';
 ?>
 
 <body onload="showText()">
+    <div id="jeux1">
+        <img src="img/Garfield.png" width="100" height="150"> <br>
+        <?php
+            $sql = "SELECT no, nom, description, prix, qte, dateParution FROM produits WHERE no = 1";
+            foreach ($conn->query($sql) as $row) {
+                echo $row['nom'] ."<br>"; 
+                echo $row['description'] ."<br>";
+                echo $row['prix'] ."$" ."<br>";
+                echo $row['qte'] ." " ."Exemplaires" ."<br>";
+            }          
+        ?>
+    </div>
+    
+    <div id="jeux2">
+        <img src="img/Mordhau.jpg" width="100" height="150"> <br>
+        <?php
+            $sql = "SELECT no, nom, description, prix, qte, dateParution FROM produits WHERE no = 2";
+            foreach ($conn->query($sql) as $row) {
+                echo $row['nom'] ."<br>"; 
+                echo $row['description'] ."<br>";
+                echo $row['prix'] ."$" ."<br>";
+                echo $row['qte'] ." " ."Exemplaires" ."<br>";
+            }          
+        ?>
+    </div>
+    
+    <div id="jeux3">
+        <img src="img/CallofDuty.jpg" width="100" height="150"> <br>
+        <?php
+            $sql = "SELECT no, nom, description, prix, qte, dateParution FROM produits WHERE no = 3";
+            foreach ($conn->query($sql) as $row) {
+                echo $row['nom'] ."<br>"; 
+                echo $row['description'] ."<br>";
+                echo $row['prix'] ."$" ."<br>";
+                echo $row['qte'] ." " ."Exemplaires" ."<br>";
+            }          
+        ?>
+    </div>
+   
+    <div id="jeux4">
+        <img src="img/Halo.png" width="100" height="150"> <br>
+        <?php
+            $sql = "SELECT no, nom, description, prix, qte, dateParution FROM produits WHERE no = 3";
+            foreach ($conn->query($sql) as $row) {
+                echo $row['nom'] ."<br>"; 
+                echo $row['description'] ."<br>";
+                echo $row['prix'] ."$" ."<br>";
+                echo $row['qte'] ." " ."Exemplaires" ."<br>";
+            }          
+        ?>
+    </div>
+    
+    <div id="jeux5">
+        <img src="img/RedDead.png" width="200" height="150"> <br>
+        <?php
+            $sql = "SELECT no, nom, description, prix, qte, dateParution FROM produits WHERE no = 3";
+            foreach ($conn->query($sql) as $row) {
+                echo $row['nom'] ."<br>"; 
+                echo $row['description'] ."<br>";
+                echo $row['prix'] ."$" ."<br>";
+                echo $row['qte'] ." " ."Exemplaires" ."<br>";
+            }          
+        ?>
+    </div>
 
-    <p id="jeux1">jeux1</p>
-
-    <p id="jeux2">jeux2</p>
-
-    <p id="jeux3">jeux3</p>
-
-    <p id="jeux4">jeux4</p>
-
-    <p id="jeux5">jeux5</p>
-
-    <p id="jeux6">jeux6</p>
-
+    <div id="jeux6">
+        <img src="img/Minecraft.jpg" width="100" height="150"> <br>
+        <?php
+            $sql = "SELECT no, nom, description, prix, qte, dateParution FROM produits WHERE no = 3";
+            foreach ($conn->query($sql) as $row) {
+                echo $row['nom'] ."<br>"; 
+                echo $row['description'] ."<br>";
+                echo $row['prix'] ."$" ."<br>";
+                echo $row['qte'] ." " ."Exemplaires" ."<br>";
+            }          
+        ?>
+    </div>
 </body>
 
 </html>
