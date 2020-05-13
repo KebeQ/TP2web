986fd2712652b97bdb35de5efb0ef77646be53ee
BloodyNightWolf <64223721+BloodyNightWolf@users.noreply.github.com>
2020-05-13 08:46:53 -0400

    Début construction

--- facture.php
+++ facture.php
@@ -1,13 +1,45 @@
-
+<?php 
+session_start();
+$_SESSION['adresseClient'] = $_POST['adresseClient'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>TP2</title>
+<title>Facture</title>
 </head>
 <body>
+<h1 class="text-center">Facture</h1>
+    <fieldset>
+    <div class="row">
+          <div class="col-md-4">
+           <?php
+           echo $_Session['adresseClient'];
+           ?>
+          </div>
+          </div>
 
+          <div class="row">
+          <div class="col-md-12">
+          <?php
+          //echo item
+          ?>
+          </div>
+          </div>
+
+          <div class="row">
+          <div class="col-md-12">
+    <label for="modePaiement">Mode de Paiement : </label>
+            <select class="form-control" id="modePaiement" name="modePaiement" required>
+              <option selected disabled value="default">---</option>
+              <option>Visa</option>
+              <option>Paypal</option>
+              <option>Mastercard</option>
+              <option>Maestro</option>
+            </select>
+            </div>
+            </div>
 </body>
 
 </html>
