0bdffe9208c7957abd25e17dc59dbdcb366344e9
BloodyNightWolf <64223721+BloodyNightWolf@users.noreply.github.com>
2020-05-13 09:31:23 -0400

    Ajout info

--- tables/commande.class.php
+++ tables/commande.class.php
@@ -9,12 +9,47 @@ class Commande {
 
     public function __construct($no, $date, $statut, $typePaiement, $noClient)
     {
-        $this->setNom($no);
-        $this->setAnneeNaissance($date);
-        $this->setNom($statut);
-        $this->setAnneeNaissance($typePaiement);
-        $this->setSolde($noClient);
+        $this->setNo($no);
+        $this->setDate($date);
+        $this->setStatut($statut);
+        $this->setTypePaiement($typePaiement);
+        $this->setNoClient($noClient);
 
+    }
+
+    public function setNo($no) {
+        $this->no = $no;        
+    }
+    public function getNo() {
+        return $this->no;
+    }
+
+    public function setDate($date) {
+        $this->date = $date;        
+    }
+    public function getDate() {
+        return $this->date;
+    }
+
+    public function setStatut($statut) {
+        $this->statut = "En traitement";        
+    }
+    public function getStatut() {
+        return $this->statut;
+    }
+
+    public function setTypePaiement($typePaiement) {
+        $this->typePaiement = $typePaiement;        
+    }
+    public function getTypePaiement() {
+        return $this->typePaiement;
+    }
+
+    public function setNoClient($noClient) {
+        $this->noClient = $noClient;        
+    }
+    public function getNoClient() {
+        return $this->noClient;
     }
 }
 
