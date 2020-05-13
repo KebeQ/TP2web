978a145d4a676039e5f6d3257808655670810f11
BloodyNightWolf <64223721+BloodyNightWolf@users.noreply.github.com>
2020-05-13 09:30:59 -0400

    Ajout infos

--- tables/items_commande.class.php
+++ tables/items_commande.class.php
@@ -7,11 +7,36 @@ class Items_Commande {
 
     public function __construct($noCommande, $date, $noProduit)
     {
-        $this->setNom($noCommande);
-        $this->setAnneeNaissance($noProduit);
-        $this->setNom($qte);
+        $this->setNoCommande($noCommande);
+        $this->setNoProduit($noProduit);
+        $this->setQuantite($qte);
+    }
+
+    public function setNoCommande($noCommande) {
+        $this->noCommande = $noCommande;        
+    }
+    public function getNoCommande() {
+        return $this->noCommande;
+    }
+
+    public function setNoProduit($noProduit) {
+        $this->noProduit = $noProduit;        
+    }
+    public function getNoProduit() {
+        return $this->noProduit;
+    }
+
+    public function setQuantite($qte) {
+        if ($qte == 0)
+            {
+            trigger_error('Désolé, ce produit est indisponible');
+            return;
 
 
+        $this->qte = $qte;        
+    }
+    public function getQuantite() {
+        return $this->qte;
     }
 }
 
