ba5a1a37bba089a312b9433825bae58ba7f98a10
BloodyNightWolf <64223721+BloodyNightWolf@users.noreply.github.com>
2020-05-13 09:33:43 -0400

    Ajout commentaire

--- panier.php
+++ panier.php
@@ -4,10 +4,12 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>TP2</title>
+<title>Panier</title>
 </head>
 <body>
-
+<?php
+//Re-regarder la vidéo; Possibilité que ceci se passe dans un array.
+?>
 </body>
 
 </html>
