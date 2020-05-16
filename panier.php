8fbc7b14df8be30b7a5b3b6682d11a54b0bb4b08
BloodyNightWolf <64223721+BloodyNightWolf@users.noreply.github.com>
2020-05-16 15:46:16 -0400

    commentaires/codes

--- panier.php
+++ panier.php
@@ -1,4 +1,13 @@
+<?php
+session_start();
+$produits= array();
+array_push($produits,  $no);
+$_SESSION['no'] = $produits;
+$enStock= array();
+array_push($enStock,  $qte);
+$_SESSION['qte'] = $enStock;
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,10 +17,11 @@
 </head>
 <body>
 <?php
-//Re-regarder la vidéo; Possibilité que ceci se passe dans un array.
-
+//Re-regarder la vidéo (la session et le array et tout se passe environ vers 23:10 et suite)
 //De plus, apparemment, le panier est le plus difficile alors il faudra s'en occuper. De plus, l'inventaire est notre classe 
 //produit dans la base de donnée. 
+//Aussi la quantité initiale est toujours 1
+//Aussi, le panier et la facture sont pareille selon la prof dans la vidéo et on a seulement besoin de copier collé (je crois)
 ?>
 </body>
 
